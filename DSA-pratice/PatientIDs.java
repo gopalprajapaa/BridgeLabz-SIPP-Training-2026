a165e8830dabb75415336c57a808ae8b91626029
Gopal <gopalprajapati8909@gmail.com>
2026-07-21 08:58:25 +0530

    mergesort,quicksort and counting sort added

---
+++ DSA-pratice/PatientIDs.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class PatientIDs {
+
+    public static void mergeSort(int arr[], int left, int right) {
+
+        if (left >= right)
+            return;
+
+        int mid = (left + right) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+
+    public static void merge(int arr[], int left, int mid, int right) {
+
+        int temp[] = new int[right - left + 1];
+
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= right) {
+
+            if (arr[i] <= arr[j])
+                temp[k++] = arr[i++];
+            else
+                temp[k++] = arr[j++];
+        }
+
+        while (i <= mid)
+            temp[k++] = arr[i++];
+
+        while (j <= right)
+            temp[k++] = arr[j++];
+
+        for (int p = 0; p < temp.length; p++)
+            arr[left + p] = temp[p];
+    }
+
+    public static void main(String args[]) {
+
+        int ids[] = {105, 101, 108, 102, 104};
+
+        mergeSort(ids, 0, ids.length - 1);
+
+        System.out.println(Arrays.toString(ids));
+    }
+}
