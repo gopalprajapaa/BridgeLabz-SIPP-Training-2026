5c122913dd7275e81e9233024a5b840a999dc76d
Gopal <gopalprajapati8909@gmail.com>
2026-07-21 09:11:25 +0530

    Binary Search questions added

---
+++ Binary_search/FirstLastOccurrence.java
@@ -0,0 +1,58 @@
+public class FirstLastOccurrence {
+
+    public static int firstOccurrence(int[] arr, int target) {
+
+        int left = 0;
+        int right = arr.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+                right = mid - 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static int lastOccurrence(int[] arr, int target) {
+
+        int left = 0;
+        int right = arr.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+                left = mid + 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 4, 4, 4, 6, 7, 8};
+
+        int target = 4;
+
+        System.out.println("First Occurrence = " + firstOccurrence(arr, target));
+        System.out.println("Last Occurrence = " + lastOccurrence(arr, target));
+    }
+}
